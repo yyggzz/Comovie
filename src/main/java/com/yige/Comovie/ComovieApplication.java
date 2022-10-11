709accb4cc72a6292abcf3a5ad1591e4926c2767
Yige <tjzhangyige@gmail.com>
2022-10-10 21:19:44 -0600

    first commit

---
+++ src/main/java/com/yige/Comovie/ComovieApplication.java
@@ -0,0 +1,13 @@
+package com.yige.Comovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yige/Comovie/ComovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yige.Comovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
